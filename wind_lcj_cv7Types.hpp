26f3fcd62903e79a19240d0065bfc6e4e900706e
Sylvain <sylvain.joyeux@tidewise.io>
2020-04-21 16:03:57 -0300

    initial commit by rock-create-orogen

---
+++ wind_lcj_cv7Types.hpp
@@ -0,0 +1,15 @@
+#ifndef wind_lcj_cv7_TYPES_HPP
+#define wind_lcj_cv7_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace wind_lcj_cv7 {
+}
+
+#endif
+
